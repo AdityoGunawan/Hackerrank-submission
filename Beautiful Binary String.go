6be921d925ebb2331f2182c27374214e7525e74f
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-10-26 19:52:17 +0700

    submission

---
+++ Beautiful Binary String.go
@@ -0,0 +1,18 @@
+package hackerranksubmission
+
+func beautifulBinaryString(b string, n int32) int32 {
+	var substring string = ""
+	var count int32
+
+	for i := 0; int32(i) < n-2; i++ {
+
+		substring += string(b[i]) + string(b[i+1]) + string(b[i+2])
+
+		if substring == "010" {
+			count += 1
+			i = i + 2
+		}
+		substring = ""
+	}
+	return count
+}
