f1a8804cd7843f00e985e5f322a573a47cd8878a
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-10-26 20:02:11 +0700

    submission

---
+++ Mars Exploration.go
@@ -0,0 +1,15 @@
+package hackerranksubmission
+
+import "fmt"
+
+func marsExploration(s string) int32 {
+	var delta int32
+	for j := range s {
+		if s[j] != "SOS"[j%3] {
+			fmt.Println("SOS"[j%3])
+			delta++
+		}
+	}
+	return delta
+
+}
